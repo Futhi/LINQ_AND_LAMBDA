28071329aee735358c9c23a508754729e05bfe2f
Futhi <futhiike@gmail.com>
2024-04-23 12:55:40 +0200

    Add project files.

---
+++ PretendCompany/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using TCPData;
+using TCPExtensions;
+
+namespace PretendCompany
+{
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //List<Employee> employeeList= Data.GetEmployees();
+
+        //var filteredEmployess = employeeList.Filter(emp => emp.AnnualSalary < 5000);
+
+        //foreach (var employee in filteredEmployess)
+        //{
+        //    Console.WriteLine($"First Name: {employee.FirstName}");
+        //    Console.WriteLine($"Last Name: {employee.LastName}");
+        //    Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+        //    Console.WriteLine($"Manager: {employee.IsManager}");
+        //    Console.WriteLine();
+        //}
+
+            //List<Department> departmentList = Data.GetDepartments();
+
+            //var filteredDepartments = departmentList.Where( dept => dept.ShortName == "TE");
+
+            //foreach (var department in filteredDepartments)
+            //{
+            //    Console.WriteLine($"Id: { department.ID}");
+            //    Console.WriteLine($"Short Name: {department.ShortName}");
+            //    Console.WriteLine($"Long Name: {department.LongName}");
+            //    Console.WriteLine();
+            //}
+
+            List<Employee> employeeList = Data.GetEmployees();    
+            List<Department> departmentList = Data.GetDepartments();
+
+            var resultList = from emp in employeeList
+                             join dept in departmentList
+                             on emp.DepartmentId equals dept.ID
+                             select new { 
+                                 FirstName = emp.FirstName ,
+                                 LastName = emp.LastName ,
+                                 AnnualSalary = emp.AnnualSalary,
+                                 Manager = emp.IsManager,
+                                 Department = dept.LongName
+                             };
+
+            foreach (var row in resultList)
+            {
+                Console.WriteLine($"First Name: {row.FirstName}");
+                Console.WriteLine($"Last Name: {row.LastName}");
+                Console.WriteLine($"Annual Salary: {row.AnnualSalary}");
+                Console.WriteLine($"Manager: {row.Manager}");
+                Console.WriteLine($"Department: {row.Department}");
+                Console.WriteLine();
+            }
+
+            var avearageAnnualSalary = resultList.Average(x => x.AnnualSalary);
+            var highestAnnualSalary = resultList.Max(x => x.AnnualSalary);
+            var lowestAnnualSalary = resultList.Min(x => x.AnnualSalary);
+
+            Console.WriteLine($"Average Salary: {avearageAnnualSalary}");
+            Console.WriteLine($"Highest Salary: {highestAnnualSalary}");
+            Console.WriteLine($"Lowest Salary: {lowestAnnualSalary}");
+
+
+
+
+
+            Console.ReadKey();
+    }
+}
+}
+
---
+++ TCPData/Data.cs
@@ -0,0 +1,102 @@
+﻿using TCPData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPData
+{
+    public static class Data
+    {
+
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee = new Employee { 
+            Id= 1,
+            FirstName= "User1",
+            LastName= "Blose",
+            AnnualSalary=2000.09m,
+            IsManager= true,
+            DepartmentId= 1
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 2,
+                FirstName = "User2",
+                LastName = "Frannk",
+                AnnualSalary = 6000.04m,
+                IsManager = true,
+                DepartmentId = 2
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 3,
+                FirstName = "User3",
+                LastName = "Jojo",
+                AnnualSalary = 8000.08m,
+                IsManager = false,
+                DepartmentId = 2
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 4,
+                FirstName = "User4",
+                LastName = "Roberts",
+                AnnualSalary = 4000.03m,
+                IsManager = true,
+                DepartmentId = 3
+            };
+
+            employees.Add(employee);
+
+            return employees; 
+        }
+
+
+        public static List<Department> GetDepartments()
+        {
+            List<Department> departmets = new List<Department>();
+
+            Department department = new Department {
+
+                ID = 1,
+                ShortName = "HR",
+                LongName= "Human Resources"
+            
+            };
+            departmets.Add(department);
+
+             department = new Department
+            {
+
+                ID = 2,
+                ShortName = "FN",
+                LongName = "Finance"
+
+            };
+            departmets.Add(department);
+
+             department = new Department
+            {
+
+                ID = 3,
+                ShortName = "TE",
+                LongName = "Technology"
+
+            };
+            departmets.Add(department);
+
+
+            return departmets;
+        }
+    }
+}
